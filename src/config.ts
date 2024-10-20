bbdc687919dce81219c9e4379ef1d8ce7342e46c
Andres Tobio <92232669+AndrewUru@users.noreply.github.com>
2024-10-20 11:45:39 +0200

    confirmar

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,4 @@
-export const SITE_TITLE = 'Astropod | Host your podcast for free';
-export const SITE_DESCRIPTION = 'Astropod is a free and open-source template for host podcast using GitHub and GitHub-Pages.';
+export const SITE_TITLE =
+  "ElSaltoWebPodCast | Plataforma de Streaming para Creadores";
+export const SITE_DESCRIPTION =
+  "ElSaltoWebPodCast es una plataforma de streaming de código abierto diseñada para que los creadores de contenido puedan tener el control total sobre su trabajo. Si estás interesado en implementar esta plataforma y ser dueño de tu contenido, contáctame en atobio459@gmail.com.";
